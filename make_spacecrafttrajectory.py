5fae7860b4c2b21f37c47666be9e9fffde37b00c
mjrutala <mrutala@bu.edu>
2023-06-23 21:14:12 +0100

    Made us of find_subset method in the Spacecraft class to select data more easily; fixed bug with converting coordinates

--- make_spacecrafttrajectory.py
+++ make_spacecrafttrajectory.py
@@ -14,27 +14,31 @@ def MSWIM2Dlike(spacecraft, starttime, stoptime):
     
     import spacecraftdata as SpacecraftData
     
-    #  Things that will be input parameters
-    #name = 'Ulysses'
-    #starttime = dt.datetime(1997, 12, 1)
-    #stoptime = dt.datetime(1998, 1, 1)
-    latitude_cutoff = 10
-    
     
     sc = SpacecraftData.SpacecraftData(spacecraft)
     sc.read_processeddata(starttime, stoptime)
     sc.find_state('SUN_INERTIAL', 'SUN')
     
+    r_range = np.array((4.5, 6.0)) 
+    lat_range = np.array((-10, 10))
+    sc.find_subset(coord1_range=r_range*sc.au_to_km, coord3_range=lat_range * np.pi / 180., transform='reclat')
     
-    rlonlat = [spice.reclat(np.array(row[['xpos', 'ypos', 'zpos']])) for index, row in sc.data.iterrows()]
+    if len(sc.data) == 0:
+        return_str = 'No spacecraft data within the given date range fall between {} and {} AU and between {} and {} deg. latitude. Returning...'
+        print(return_str.format(str(r_range[0]), str(r_range[1]), str(lat_range[0]), str(lat_range[1])))       
+        
+        return()
+    
+    # rlonlat = list()
+    # for index, row in sc.data.iterrows():
+    #     print(row)
+    #     print(type(row))
+    #     dummy1 = row[['x_pos', 'y_pos', 'z_pos']]
+    #     dummy2 = np.array(dummy1, dtype='float64')
+    #     dummy3 = spice.reclat(dummy2)
+    #     rlonlat.append(dummy3)
+    rlonlat = [spice.reclat(np.array(row[['x_pos', 'y_pos', 'z_pos']], dtype='float64')) for index, row in sc.data.iterrows()]
     rlonlat = np.array(rlonlat)
-    
-    criteria_index = np.where((rlonlat[:,0]/sc.au_to_km >= 4.5) 
-                              & (rlonlat[:,0]/sc.au_to_km <= 5.5) 
-                              & (rlonlat[:,2]*180/np.pi > -latitude_cutoff) 
-                              & (rlonlat[:,2]*180/np.pi < latitude_cutoff))
-    
-    sc.data = sc.data.iloc[criteria_index]
     
     df_to_write = pd.DataFrame()
     df_to_write['year'] = [t.strftime('%Y') for t in sc.data.index]
@@ -44,14 +48,14 @@ def MSWIM2Dlike(spacecraft, starttime, stoptime):
     df_to_write['mn'] = [t.strftime('%M') for t in sc.data.index]
     df_to_write['sc'] = [t.strftime('%S') for t in sc.data.index]
     df_to_write['msc'] = [t.strftime('%f')[:-3] for t in sc.data.index] # micros to millis
-    df_to_write['x'] = [coord/sc.au_to_km for coord in sc.data['xpos']]
-    df_to_write['y'] = [coord/sc.au_to_km for coord in sc.data['ypos']]
-    df_to_write['z'] = [coord/sc.au_to_km for coord in sc.data['zpos']]
+    df_to_write['x'] = [coord/sc.au_to_km for coord in sc.data['x_pos']]
+    df_to_write['y'] = [coord/sc.au_to_km for coord in sc.data['y_pos']]
+    df_to_write['z'] = [coord/sc.au_to_km for coord in sc.data['z_pos']]
     
     trajectory_filename = ''.join([sc.name.lower(), '_trajectory_',
-                                   sc.data.index[0].strftime('%Y%m%d%H%M%S'),
+                                   starttime.strftime('%Y%m%d%H%M%S'),
                                    '-', 
-                                   sc.data.index[-1].strftime('%Y%m%d%H%M%S'), 
+                                   stoptime.strftime('%Y%m%d%H%M%S'),
                                    '.dat'])
     
     with open(trajectory_filename, "w") as file:
