426f3acb82dde93f8364dfb38ab49f0a7695039c
mjrutala <mrutala@bu.edu>
2023-07-03 16:02:07 +0100

    Added a data downloader, currently set up for Pioneers 10 and 11, and added the Pioneer() program to read pioneer data into a dataframe

--- read_SWData.py
+++ read_SWData.py
@@ -14,6 +14,101 @@ import numpy as np
 import os
 import astropy.units as u
  
+from pathlib import Path
+
+# =============================================================================
+# 
+# =============================================================================
+def get_fromCDAWeb(spacecraft, basedir='',
+                   plasma=False, mag=False, merged=True):
+    import subprocess
+    import os
+    import glob
+    
+    import pandas as pd
+    
+    baseurl = 'https://cdaweb.gsfc.nasa.gov/pub/data/'
+    
+    basedir = Path(basedir)
+    #
+    path_info = pd.DataFrame(columns=['plasma', 'mag', 'merged'], 
+                             index=['url', 'savedir', 'filename_pattern'])
+    
+    #  Construct os-agnostic destination paths 
+    # =============================================================================
+    #     
+    # =============================================================================
+    match spacecraft.lower():
+        case 'pioneer 10':
+            spacecraft_family = 'pioneer'
+            spacecraft_dir = basedir / spacecraft_family / spacecraft.lower().replace(' ','')
+            #plasma_instrument = ''
+            #mag_instrument = ''
+            
+            # #  Plasma data
+            # plasma_url = '/pioneer/pioneer10/plasma/version31/v31_hour_ascii/'
+            # #plasma_url = '/pioneer/pioneer10/plasma/version31/v31_summary_ascii/'
+            # plasma_savedir = 
+            # plasma_filename_pattern = 'p10plasm_????.asc'
+            
+            # #  Mag data
+            # mag_url = '/pioneer/pioneer10/mag/ip_1min_ascii/'
+            # mag_savedir = 
+            # mag_filename_pattern = 'm?????.asc'
+            
+            #  Hourly merged (plasma + mag) data
+            path_info['merged']['url'] = 'pioneer/pioneer10/merged/coho1hr_magplasma_ascii/'
+            path_info['merged']['savedir'] = spacecraft_dir / 'merged/hourly/'
+            path_info['merged']['filename_pattern'] = 'p10_????.asc' #  'p10_%Y.asc'
+            
+        case 'pioneer 11':
+            spacecraft_family = 'pioneer'
+            spacecraft_dir = basedir / spacecraft_family / spacecraft.lower().replace(' ','')
+            #  Hourly merged (plasma + mag) data
+            path_info['merged']['url'] = 'pioneer/pioneer11/merged/coho1hr_magplasma_ascii/'
+            path_info['merged']['savedir'] = spacecraft_dir / 'merged/hourly/'
+            path_info['merged']['filename_pattern'] = 'p11_????.asc' # 'p11_%Y.asc'
+            
+        case _:
+            result = None
+            return(result)
+    
+    # =============================================================================
+    #     
+    # =============================================================================
+    commandname = ['wget']
+    
+    if not plasma: path_info.drop('plasma', axis=1, inplace=True)
+    if not mag: path_info.drop('mag', axis=1, inplace=True)
+    if not merged: path_info.drop('merged', axis=1, inplace=True)
+    
+    for column in path_info:
+        
+        if not os.path.exists(path_info[column]['savedir']):
+            os.makedirs(path_info[column]['savedir'])
+        
+        #  A bunch of flags with this command call:
+        #  -r: recursive
+        #  -R *.html: reject .html files
+        #  -np: don't ascend to the parent directory and go looking for files
+        #  --directory-prefix: where to copy the files to
+        #  -nH: no host directory (i.e. 'https://...')
+        #  -nd: copy all files to this directory (could be --cut-dirs=#...)
+        #  -s-show_progress: give a loading bar showing download time
+        #  -q: quiet, don't show any other info
+        flags = ['-r', '-R', '*.html', '-np', 
+                  '--directory-prefix=' + str(path_info[column]['savedir']), 
+                  '-nH', '-nd', '--show-progress', '-q']
+        filepattern_flags = ['--accept=' + path_info[column]['filename_pattern']]
+        host_url = [baseurl + path_info[column]['url']]
+    
+        commandline = commandname + flags + filepattern_flags + host_url
+        
+        subprocess.run(commandline)
+        
+    return()
+
+
 # =============================================================================
 # 
 # =============================================================================
@@ -407,6 +502,192 @@ def Voyager(starttime, stoptime, spacecraft_number=1, basedir=None):
     #data.attrs['equations'] = {**plasma_data.attrs['equations'], **mag_data.attrs['equations']}
         
     return(data)
+
+def Pioneer(starttime, stoptime, spacecraft_number=10, basedir=''):
+    #  Keep basefilepath flexible such that it can point to a static path or a 
+    #  relative path
+    #  Default to current working directory
+    
+    basedir = Path(basedir)
+    
+    spacecraft_number = str(spacecraft_number).strip()
+    spacecraft_dir = 'pioneer/pioneer' + spacecraft_number + '/' 
+    
+    filepath_ions =  basedir / spacecraft_dir / 'plasma/'
+    filepath_mag = basedir / spacecraft_dir / 'mag/'
+    filepath_merged = basedir / spacecraft_dir / 'merged/hourly/'
+    
+    def read_Pioneer_plasma(starttime, finalttime):
+        #  List only the relevant files by generating filenames
+        filename_template_ion = 'p'+spacecraft_numer+'_%Y.asc'
+        in_range = np.arange(starttime.year, stoptime.year+1, 1)                                       # !!! Repeated
+        filename_datetimes_in_range = [dt.datetime(iyr, 1, 1) for iyr in in_range]                      # !!! Repeated
+        filenames_in_range = [t.strftime(filename_template_ion) for t in filename_datetimes_in_range]   # !!! Repeated
+        
+        filelist = [filepath_ions + filename for filename in filenames_in_range if os.path.exists(filepath_ions + filename)]
+        filelist.sort()
+        
+        #  Read all the data into one dataframe
+        input_columns = ['iyr', 'idoy', 'ihr', 'imin', 'isec',
+                         'rau', 'hlat', 'hlong', 
+                         'n_proton', 'n_alpha', 
+                         'T_large', 'T_small', 
+                         'u_r', 'u_t', 'u_n']
+        
+        #  Make a generator to read csv data and concatenate into a single dataframe
+        data_generator = (pd.read_csv(f, delim_whitespace=True, header=0, names=input_columns) 
+                                     for f in filelist)
+        spacecraft_data = pd.concat(data_generator, ignore_index=True)
+        
+        
+        datetime_generator = ('{:02n}-{:03n}T{:02n}:{:02n}:{:02n}'
+                              .format(*row[['iyr', 'idoy', 'ihr', 'imin', 'isec']])
+                              for indx, row in spacecraft_data.iterrows())
+            
+        spacecraft_data.index = [dt.datetime.strptime(
+                                 gen, '%y-%jT%H:%M:%S') 
+                                 for gen in datetime_generator]
+        
+        spacecraft_data = spacecraft_data.drop(['iyr', 'idoy', 'ihr', 'imin', 'isec'], axis=1)
+        
+        spacecraft_data['u_mag'] = np.sqrt(np.sum(spacecraft_data[['u_r', 'u_t', 'u_n']]**2., axis=1))
+        #spacecraft_data['rss'] = np.sqrt(spacecraft_data.u_r**2 + spacecraft_data.u_t**2 + spacecraft_data.u_n**2)
+        
+        
+        defaultunits_to_nPa = ((1.0 * u.M_p / u.cm**3) * (u.km**2 / u.s**2)).to(u.nPa).value
+        
+        spacecraft_data['p_dyn_proton'] = spacecraft_data['n_proton'] * spacecraft_data['u_mag']**2. * defaultunits_to_nPa
+        
+        spacecraft_data['p_dyn_alpha'] = 4. * spacecraft_data['n_alpha'] * spacecraft_data['u_mag']**2. * defaultunits_to_nPa
+        
+        spacecraft_data['p_dyn'] = spacecraft_data['p_dyn_proton'] #  !!!
+        #  Within selected time range
+        time_index = np.where((spacecraft_data.index >= starttime) 
+                      & (spacecraft_data.index < stoptime))
+        spacecraft_data = spacecraft_data.iloc[time_index]
+        
+        #  Check for duplicates in the datetime index
+        spacecraft_data = spacecraft_data[~spacecraft_data.index.duplicated(keep='last')]
+        
+        output_columns = ['rau', 'hlat', 'hlong', 'n_proton', 'n_alpha', 'T_large', 'T_small', 'u_r', 'u_t', 'u_n', 'p_dyn_proton', 'p_dyn_alpha']
+        output_columns_units = ['AU', 'deg', 'deg', 'cm^{-3}', 'cm^{-3}', 'K', 'K', 'km/s', 'km/s', 'km/s', 'nPa', 'nPa']
+        spacecraft_data.attrs['units'] = dict(zip(output_columns, output_columns_units))
+        
+        output_columns_eqns = ['', '', '', '', '', '', '', '', '', '', '0.5*n_proton*u_mag**2', '0.5*n_alpha*u_mag**2']
+        spacecraft_data.attrs['equations'] = dict(zip(output_columns, output_columns_eqns))
+        
+        return(spacecraft_data)
+    
+    def read_Pioneer_MAG(starttime, stoptime):
+        filename_template = 'ulymaghr_%Y.asc'
+        
+        in_range = np.arange(starttime.year, stoptime.year+1, 1)                                       # !!! Repeated
+        filename_datetimes_in_range = [dt.datetime(iyr, 1, 1) for iyr in in_range]                      # !!! Repeated
+        filenames_in_range = [t.strftime(filename_template) for t in filename_datetimes_in_range]   # !!! Repeated
+        
+        filelist = [filepath_mag + filename for filename in filenames_in_range if os.path.exists(filepath_mag + filename)]
+        filelist.sort()
+        
+        input_columns = ['iyr', 'iddoy', 'idhr',  #  decimal doy, decimal hour
+                         'B_r', 'B_t', 'B_n', 'B_mag',  
+                         'nBVectors']
+        
+        #  Make a generator to read csv data and concatenate into a single dataframe
+        data_generator = (pd.read_csv(f, delim_whitespace=True, header=0, names=input_columns) 
+                                     for f in filelist)
+        spacecraft_data = pd.concat(data_generator, ignore_index=True)
+        
+        spacecraft_data['idoy'] = [int(iddoy) for iddoy in spacecraft_data['iddoy']]
+        spacecraft_data['ihr'] = [int(idhr) for idhr in spacecraft_data['idhr']]
+        spacecraft_data['imin'] = [int((idhr % 1) * 60.) for idhr in spacecraft_data['idhr']]
+        spacecraft_data['isec'] = [int((((idhr % 1) * 60.) % 1) * 60.) for idhr in spacecraft_data['idhr']]
+        datetime_generator = ('{:02n}-{:03n}T{:02n}:{:02n}:{:02n}'
+                              .format(*row[['iyr', 'idoy', 'ihr', 'imin', 'isec']])
+                              for indx, row in spacecraft_data.iterrows())
+        spacecraft_data.index = [dt.datetime.strptime(
+                                 gen, '%y-%jT%H:%M:%S') 
+                                 for gen in datetime_generator]
+        spacecraft_data = spacecraft_data.drop(['iyr', 'idoy', 'iddoy', 'ihr', 'idhr', 'imin', 'isec'], axis=1)
+        
+        #  Within selected time range
+        time_index = np.where((spacecraft_data.index >= starttime) 
+                      & (spacecraft_data.index < stoptime))
+        spacecraft_data = spacecraft_data.iloc[time_index]
+        
+        #  Check for duplicates in the datetime index
+        spacecraft_data = spacecraft_data[~spacecraft_data.index.duplicated(keep='last')]
+        
+        output_columns = ['B_r', 'B_t', 'B_n', 'B_mag', 'nBVectors']
+        output_columns_units = ['nT', 'nT', 'nT', 'nT', '#']
+        spacecraft_data.attrs['units'] = dict(zip(output_columns, output_columns_units))
+        
+        output_columns_eqns = ['', '', '', '', '']
+        spacecraft_data.attrs['equations'] = dict(zip(output_columns, output_columns_eqns))
+        
+        return(spacecraft_data)
+    
+    def read_Pioneer_merged(starttime, finalttime):
+        #  List only the relevant files by generating filenames
+        filename_template_ion = 'p'+spacecraft_number+'_%Y.asc'
+        in_range = np.arange(starttime.year, stoptime.year+1, 1)                                       # !!! Repeated
+        filename_datetimes_in_range = [dt.datetime(iyr, 1, 1) for iyr in in_range]                      # !!! Repeated
+        filenames_in_range = [t.strftime(filename_template_ion) for t in filename_datetimes_in_range]   # !!! Repeated
+        
+        filelist = [filepath_merged / filename for filename in filenames_in_range if os.path.exists(filepath_merged / filename)]
+        filelist.sort()
+        
+        #  Read all the data into one dataframe
+        input_columns = ['iyr', 'idoy', 'ihr',
+                         'rau', 'hgi_lat', 'hgi_lon', 
+                         'B_r', 'B_t', 'B_n', 'B_mag',
+                         'u_mag_proton', 'u_theta_proton', 'u_phi_proton',
+                         'n_proton', 
+                         'T_proton',
+                         'proton_flux_low', 'proton_flux_mid', 'proton_flux_high']
+        
+        #  Make a generator to read csv data and concatenate into a single dataframe
+        data_generator = (pd.read_csv(f, delim_whitespace=True, names=input_columns,
+                                      na_values=['999.99', '9999.9', '999.9999',  '9999999.', '9.999999e+06']) 
+                                     for f in filelist)
+        spacecraft_data = pd.concat(data_generator, ignore_index=True)
+        
+        datetime_generator = ('{:02n}-{:03n}T{:02n}:00:00'
+                              .format(*row[['iyr', 'idoy', 'ihr']])
+                              for indx, row in spacecraft_data.iterrows())
+            
+        spacecraft_data.index = [dt.datetime.strptime(
+                                 date, '%Y-%jT%H:%M:%S') 
+                                 for date in datetime_generator]
+        
+        spacecraft_data = spacecraft_data.drop(['iyr', 'idoy', 'ihr'], axis=1)
+        
+        spacecraft_data['u_mag'] = spacecraft_data['u_mag_proton'] # !!!!
+        
+        defaultunits_to_nPa = ((1.0 * u.M_p / u.cm**3) * (u.km**2 / u.s**2)).to(u.nPa).value
+        
+        spacecraft_data['p_dyn_proton'] = spacecraft_data['n_proton'] * spacecraft_data['u_mag']**2. * defaultunits_to_nPa
+        
+        spacecraft_data['p_dyn'] = spacecraft_data['p_dyn_proton'] #  !!!
+        
+        #  Within selected time range
+        time_index = np.where((spacecraft_data.index >= starttime) 
+                      & (spacecraft_data.index < stoptime))
+        spacecraft_data = spacecraft_data.iloc[time_index]
+        
+        #  Check for duplicates in the datetime index
+        spacecraft_data = spacecraft_data[~spacecraft_data.index.duplicated(keep='last')]
+        
+        return(spacecraft_data)
+    # =============================================================================
+    #     
+    # =============================================================================
+    
+    merge_data = read_Pioneer_merged(starttime, stoptime)
+    
+    data = pd.concat([merge_data], axis=1)
+    
+    return(data)
+
 # =============================================================================
 # A simple function which allows you to specify the spacecraft name as an argument
 # Which then calls the relrevant functions
@@ -422,6 +703,10 @@ def read(spacecraft, starttime, stoptime, basedir=None):
             result = Voyager(starttime, stoptime, spacecraft_number='1', basedir=basedir)
         case 'voyager 2':
             result = Voyager(starttime, stoptime, spacecraft_number='2', basedir=basedir)
+        case 'pioneer 10':
+            result = Pioneer(starttime, stoptime, spacecraft_number='10', basedir=basedir)
+        case 'pioneer 11':
+            result = Pioneer(starttime, stoptime, spacecraft_number='11', basedir=basedir)
         case _:
             result = None
     return(result)
