4c7d17d263b2a9ddd23a9511b238e85f2de9eec8
mjrutala <mrutala@bu.edu>
2024-09-17 00:31:33 +0100

    MMESH_context now includes 13-month-smoothed Solar F10.7 flux, solar recurrence index based on OMNIflow speed, and 13-month-smoothed solar F10.7 flux as predictors

--- mmesh/MMESH_context.py
+++ mmesh/MMESH_context.py
@@ -12,8 +12,10 @@ import spiceypy as spice
 
 #   !!!! To set up file
 kernel_paths = ['/Users/mrutala/SPICE/generic/metakernel_planetary.txt',
-                '/Users/mrutala/SPICE/juno/metakernel_juno.txt']
+                '/Users/mrutala/SPICE/juno/metakernel_juno.txt',
+                '/Users/mrutala/SPICE/ulysses/metakernel_ulysses.txt']
 SolarRadioFlux_filepath = '/Users/mrutala/Data/Sun/DRAO/penticton_radio_flux.csv'
+OMNI_filedir = '/Users/mrutala/Data/OMNI/LowRes/'
 
 
 #   MMESH should read DataFrames in from CSV
@@ -31,6 +33,9 @@ def read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min'):
                                 for d in srf_df['date']]
     srf_df = srf_df.set_index('date')
     srf_df = srf_df.resample(sample_rate, origin='start_day').mean().interpolate(method='linear')
+
+    #   Also take an 13-rotation rolling mean
+    srf_df['smooth_adjusted_flux'] = srf_df['adjusted_flux'].rolling(dt.timedelta(days=13*27), center=True, min_periods=13*27*24).mean()
 
     result = srf_df[(srf_df.index >= starttime) & (srf_df.index <= stoptime)]
     
@@ -59,9 +64,12 @@ def make_PlanetaryContext_CSV(target, starttime, stoptime, kernel_paths=kernel_p
     del_lat_TSE = (target_rlonlat[:,2] - earth_rlonlat[:,2])*180/np.pi
     
     context_df = pd.DataFrame(index=pd.DatetimeIndex(datetimes))
+    context_df.loc[:, 'target_sun_earth_r'] = np.abs(del_r_TSE)
     context_df.loc[:, 'target_sun_earth_lon'] = np.abs(np.abs(del_lon_TSE + 180) % 360 - 180)
     context_df.loc[:, 'target_sun_earth_lat'] = np.abs(np.abs(del_lat_TSE + 180) % 360 - 180)
     context_df.loc[:, 'solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['adjusted_flux']
+    context_df.loc[:, 'smooth_solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['smooth_adjusted_flux']
+    context_df.loc[:, 'recurrence_index'] = find_RecurrenceIndex_fromOMNI(starttime, stoptime, sample_rat='60Min', smooth=True)
     
     filename = '{}_{}-{}_Context.csv'.format(target.lower().capitalize(),
                                              starttime.strftime('%Y%m%d%H%M%S'),
@@ -87,15 +95,19 @@ def make_SpacecraftContext_CSV(target, starttime, stoptime, kernel_paths=kernel_
     earth_rlonlat = np.array([spice.reclat(xyz) for xyz in earth_xyz])
     
     spice.kclear()
-    print(ets)
+    
     del_r_TSE = target_rlonlat[:,0] - earth_rlonlat[:,0]
     del_lon_TSE = (target_rlonlat[:,1] - earth_rlonlat[:,1])*180/np.pi
     del_lat_TSE = (target_rlonlat[:,2] - earth_rlonlat[:,2])*180/np.pi
     
     context_df = pd.DataFrame(index=pd.DatetimeIndex(datetimes))
+    context_df.loc[:, 'target_sun_earth_r'] = np.abs(del_r_TSE)
     context_df.loc[:, 'target_sun_earth_lon'] = np.abs(np.abs(del_lon_TSE + 180) % 360 - 180)
     context_df.loc[:, 'target_sun_earth_lat'] = np.abs(np.abs(del_lat_TSE + 180) % 360 - 180)
     context_df.loc[:, 'solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['adjusted_flux']
+    context_df.loc[:, 'smooth_solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['smooth_adjusted_flux']
+    context_df.loc[:, 'recurrence_index'] = find_RecurrenceIndex_fromOMNI(starttime, stoptime, sample_rate='60Min', smooth=True)
+    
     
     filename = '{}_{}-{}_Context.csv'.format(target.lower().capitalize(),
                                              starttime.strftime('%Y%m%d%H%M%S'),
@@ -103,3 +115,51 @@ def make_SpacecraftContext_CSV(target, starttime, stoptime, kernel_paths=kernel_
     fullfilepath = filepath / filename
     context_df.to_csv(str(fullfilepath), index_label='datetime')
     return fullfilepath
+
+def find_RecurrenceIndex_fromOMNI(starttime, stoptime, smooth=True, sample_rate='60Min'):
+    import glob
+    import datetime
+    
+    filepaths = glob.glob(OMNI_filedir + 'omni_m????.dat')
+    
+    filepaths = sorted(filepaths)
+    
+    column_names = ['year', 'ddoy', 'hour', 
+                    'lat_HGI', 'lon_HGI', 
+                    'Br', 'Bt', 'Bn', 'B_avg', 
+                    'U_sw', 'U_elevation_angle', 'U_azimuth_angle', 
+                    'n_proton', 
+                    'T']
+    df_list = []
+    for filepath in filepaths:
+        df = pd.read_csv(filepath, sep='\s+', names=column_names)
+        df_list.append(df)
+        
+    df = pd.concat(df_list, ignore_index=True)
+    datetime_strings = ["{:04g}-{:03g}-{:02g}".format(*row[['year', 'ddoy', 'hour']].values) for _, row in df.iterrows()]
+    df.index = [datetime.datetime.strptime(d, "%Y-%j-%H") for d in datetime_strings]
+    
+    df['U_sw_shifted'] = df['U_sw'].shift(-27, freq='D')
+    
+    #   Calculating the recurrence index only requires the bulk flow
+    #   speed of the solar wind plasma
+    u_df = df.loc[:, ['U_sw', 'U_sw_shifted']]
+    
+    #   Calculate the corr. coeff. matrix for each pair
+    corr_df = u_df.rolling(datetime.timedelta(days=27), center=True, min_periods=27*24).corr(pairwise=True)
+    
+    #   Isolate the corr. coeff. between U_sw and U_sw_shifted
+    corr_df = corr_df.xs('U_sw', axis='index', level=1)['U_sw_shifted'].to_frame(name='corr')
+    
+    #   Also take an 13-rotation rolling mean
+    corr_df['smooth_corr'] = corr_df.rolling(datetime.timedelta(days=13*27), center=True, min_periods=13*27*24).mean()
+    
+    #   Subset both to lie between starttime (closed) and stoptime (open)
+    corr_df = corr_df.query('@starttime <= index < @stoptime')
+    
+    result = corr_df['corr'].to_numpy()
+    if smooth:
+        result = corr_df['smooth_corr'].to_numpy()
+    
+    return result
+    
