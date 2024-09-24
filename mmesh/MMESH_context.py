50e8a17fa7927f60dee6298cba8af047877a80d7
mjrutala <mrutala@bu.edu>
2024-09-24 13:17:47 +0100

    Added SolarCycleSmoother() routine to take 13-month rolling averages, with half-weights on the first and final months, of context parameters-- used in smoothing SRF and calcualting solar recurrence index

--- mmesh/MMESH_context.py
+++ mmesh/MMESH_context.py
@@ -24,7 +24,58 @@ OMNI_filedir = '/Users/mrutala/Data/OMNI/LowRes/'
 #   And it should be safe to assume the end user can, at worst, copy and paste things into
 #   a formatted spreadsheet and run from there
 
+def SolarCycleSmoother(series, resolution = 3600):
+    """
+    Smooth paramters as appropriate for solar cycle    
 
+    Parameters
+    ----------
+    series : pandas Series
+        Series to be smoothed. Must have datetimeindex-type index.
+    resolution : int or float, optional
+        Temporal resolution, in seconds. The default is 3600.
+
+    Returns
+    -------
+    result : pandas Series
+        Output smoothed series with same index as series
+
+    """
+    
+    #   Get some numbers
+    len_CR = 27     # Carrington rotation period, earth days
+    n_CR = 13       # Number of Carring rotations to average over
+    conversion_factor = int(24*60*60 / resolution) # Convert from days to seconds, then divide by resolution in seconds to get days resolution elements per dayå
+    
+    len_kernel = int(n_CR * len_CR * conversion_factor)
+    half_len_kernel = int(len_kernel/2)
+    
+    #   Make a convolution kernel
+    kernel = np.zeros(len_kernel) + 2
+    kernel[:len_CR * conversion_factor] = 1
+    kernel[-len_CR * conversion_factor:] = 1
+    kernel = kernel/np.sum(kernel)
+    
+    #   Resample series so that it is evenly spaced (just in case)
+    s = series.resample(dt.timedelta(seconds=resolution)).mean()
+    
+    #   Convert series to array, mirror both edges by half the kernel to prevent boundary effects
+    a = np.zeros(int(len(s)) + len_kernel, dtype='float64')
+    a[half_len_kernel:-half_len_kernel] = s
+    a[:half_len_kernel] = np.flip(s[:half_len_kernel]).to_numpy()
+    a[-half_len_kernel:] = np.flip(s[-half_len_kernel:]).to_numpy()
+    
+    #   Convolve
+    c = np.convolve(a, kernel, 'same')
+    
+    #   Put the result back into a series
+    result = pd.Series(c[half_len_kernel:-half_len_kernel], 
+                       index = s.index)
+    
+    return result
+
+    
+    
 def read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min'):
     column_headers = ('date', 'observed_flux', 'adjusted_flux',)
     srf_df = pd.read_csv(SolarRadioFlux_filepath,
@@ -35,7 +86,7 @@ def read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min'):
     srf_df = srf_df.resample(sample_rate, origin='start_day').mean().interpolate(method='linear')
 
     #   Also take an 13-rotation rolling mean
-    srf_df['smooth_adjusted_flux'] = srf_df['adjusted_flux'].rolling(dt.timedelta(days=13*27), center=True, min_periods=13*27*24).mean()
+    srf_df['smooth_adjusted_flux'] = SolarCycleSmoother(srf_df['adjusted_flux'])
 
     result = srf_df[(srf_df.index >= starttime) & (srf_df.index <= stoptime)]
     
@@ -106,7 +157,7 @@ def make_SpacecraftContext_CSV(target, starttime, stoptime, kernel_paths=kernel_
     context_df.loc[:, 'target_sun_earth_lat'] = np.abs(np.abs(del_lat_TSE + 180) % 360 - 180)
     context_df.loc[:, 'solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['adjusted_flux']
     context_df.loc[:, 'smooth_solar_radio_flux'] = read_SolarRadioFlux(starttime, stoptime, sample_rate='60Min')['smooth_adjusted_flux']
-    context_df.loc[:, 'recurrence_index'] = find_RecurrenceIndex_fromOMNI(starttime, stoptime, sample_rate='60Min', smooth=True)
+    context_df.loc[:, 'recurrence_index'] = find_RecurrenceIndex_fromOMNI(starttime, stoptime, sample_rate='60Min')['recurrence_index']
     
     
     filename = '{}_{}-{}_Context.csv'.format(target.lower().capitalize(),
@@ -116,14 +167,15 @@ def make_SpacecraftContext_CSV(target, starttime, stoptime, kernel_paths=kernel_
     context_df.to_csv(str(fullfilepath), index_label='datetime')
     return fullfilepath
 
-def find_RecurrenceIndex_fromOMNI(starttime, stoptime, smooth=True, sample_rate='60Min'):
+def find_RecurrenceIndex_fromOMNI(starttime, stoptime, sample_rate='60Min'):
     import glob
     import datetime
     
+    #   Search for files, and return a sorted list
     filepaths = glob.glob(OMNI_filedir + 'omni_m????.dat')
-    
     filepaths = sorted(filepaths)
     
+    #   Read all the found files into a dataframe
     column_names = ['year', 'ddoy', 'hour', 
                     'lat_HGI', 'lon_HGI', 
                     'Br', 'Bt', 'Bn', 'B_avg', 
@@ -139,27 +191,24 @@ def find_RecurrenceIndex_fromOMNI(starttime, stoptime, smooth=True, sample_rate=
     datetime_strings = ["{:04g}-{:03g}-{:02g}".format(*row[['year', 'ddoy', 'hour']].values) for _, row in df.iterrows()]
     df.index = [datetime.datetime.strptime(d, "%Y-%j-%H") for d in datetime_strings]
     
+    #   Calculating the recurrence index only requires the bulk flow speed
+    df = df['U_sw'].to_frame()
     df['U_sw_shifted'] = df['U_sw'].shift(-27, freq='D')
     
-    #   Calculating the recurrence index only requires the bulk flow
-    #   speed of the solar wind plasma
-    u_df = df.loc[:, ['U_sw', 'U_sw_shifted']]
-    
     #   Calculate the corr. coeff. matrix for each pair
-    corr_df = u_df.rolling(datetime.timedelta(days=27), center=True, min_periods=27*24).corr(pairwise=True)
+    corr_df = df.rolling(datetime.timedelta(days=27), center=True).corr(pairwise=True)
     
     #   Isolate the corr. coeff. between U_sw and U_sw_shifted
-    corr_df = corr_df.xs('U_sw', axis='index', level=1)['U_sw_shifted'].to_frame(name='corr')
+    corr_df = corr_df.xs('U_sw', axis='index', level=1)['U_sw_shifted'].to_frame(name='correlation_coefficient')
     
-    #   Also take an 13-rotation rolling mean
-    corr_df['smooth_corr'] = corr_df.rolling(datetime.timedelta(days=13*27), center=True, min_periods=13*27*24).mean()
+    #   Also take a 13-rotation rolling mean
+    corr_df['recurrence_index'] = SolarCycleSmoother(corr_df['correlation_coefficient'].dropna())
     
     #   Subset both to lie between starttime (closed) and stoptime (open)
-    corr_df = corr_df.query('@starttime <= index < @stoptime')
+    results_df = pd.DataFrame(index = pd.date_range(starttime, stoptime, freq='60Min'),
+                              columns = corr_df.columns)
+    overlap_indx = results_df.index.intersection(corr_df.index)
+    results_df.loc[overlap_indx, :] = corr_df.loc[overlap_indx, :]
     
-    result = corr_df['corr'].to_numpy()
-    if smooth:
-        result = corr_df['smooth_corr'].to_numpy()
-    
-    return result
+    return results_df
     
